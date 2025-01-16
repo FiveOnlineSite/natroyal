ff0d431758189b0dbc3bb287c1c5531042d4a082
AdarshFiveonline <helpdesk@fiveonline.in>
2025-01-16 15:52:50 +0530

    added new changes

--- frontend/src/pages/Company.js
+++ frontend/src/pages/Company.js
@@ -608,6 +608,54 @@ const Company = () => {
               </div>
             </div>
           </div>
+
+          <div className="row company-row">
+            <div className="col-lg-12">
+              <div className="row mt-5 align-items-center justify-content-center">
+                <div className="col-lg-6 company-bg">
+                  <div className="company-img">
+                    <img
+                      src="/images/banners/WeTransformSpaces-qx19auns89zyddt5mo4mrb7761rnpqi25vb42khzpw.webp"
+                      alt="company-img"
+                    />
+                    <div className="logo-gray-div">
+                      {/* <img src="/images/icons/image 46.png" alt="logo" /> */}
+                      <h2>LVT</h2>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-6 ps-lg-5 ps-lg-0 ps-auto pt-lg-0 pt-5">
+                  <div className="companies-content">
+                    <h6 className="subtitle">
+                      Vinyl Plank & Tiles <span></span>
+                    </h6>
+                    <h2 className="title">Luxury Vinyl Plank & Tiles (LVT)</h2>
+
+                    <p className="paragraph mb-5">
+                      Natroyal Group (formerly Samsons Group) is a pioneer in
+                      India, manufacturing and supplying luxury vinyl planks and
+                      tiles for various industries, including education,
+                      healthcare, residential, hospitality, office/retail, and
+                      transportation sectors.
+                    </p>
+
+                    <NavLink
+                      to="/"
+                      target="_blank"
+                      className="custom-button border-btn"
+                    >
+                      View More
+                      <img
+                        src="/images/icons/arrow-up-right.png"
+                        className="ps-2"
+                        alt="arrow"
+                      />
+                    </NavLink>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
 
